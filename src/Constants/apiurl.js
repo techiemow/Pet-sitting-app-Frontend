24f7170f2295229e99b2e48dbde994001d9bf6ce
techiemow <158059327+techiemow@users.noreply.github.com>
2024-08-09 12:08:04 +0530

    changes apiurl.js

--- src/Constants/apiurl.js
+++ src/Constants/apiurl.js
@@ -1,4 +1,4 @@
 export const apiurl =
   import.meta.env.VITE_LOCAL_URL === "production"
-    ? "https://paws-and-claws-61ks.onrender.com"
+    ? "http://localhost:4000"
     : `http://localhost:4000`;
